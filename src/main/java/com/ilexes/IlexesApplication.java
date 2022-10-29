4dbaeb872587fc1c3a0117d6931bcb89bc86e717
Viktor Aleksandrov <viktorostrov07@gmail.com>
2022-10-30 00:24:47 +0300

    initial commit

---
+++ src/main/java/com/ilexes/IlexesApplication.java
@@ -0,0 +1,13 @@
+package com.ilexes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlexesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlexesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ilexes/IlexesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilexes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlexesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
