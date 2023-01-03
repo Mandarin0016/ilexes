e05e6771148cfcdbb411322abe6e06de50aa6218
PeterMirchev <119004862+PeterMirchev@users.noreply.github.com>
2023-01-03 23:05:36 +0200

    updated userRepository

--- src/main/java/com/ilexes/repository/UserRepository.java
+++ src/main/java/com/ilexes/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.ilexes.repository;
 
-public interface UserRepository {
+import com.ilexes.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository <User, Long> {
+
 }
